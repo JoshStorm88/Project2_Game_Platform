fdcdec344b3ff2c2167863663f8f129dbf9d0140
Josh Storm <joshua_taljaard@outlook.com>
2018-06-05 10:02:24 +0100

    added User buttons to nav bar

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: { type: String, unique: true, required: true},
+  password: { type: String, required: true },
+  pictures: [{type: mongoose.Schema.Types.ObjectId, ref: 'Picture'}]
+},{
+  timestamps: true
+});
+
+
+
+userSchema.methods.validatePassword = function(password){
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+
+  next();
+});
+
+userSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+
+userSchema.pre('validate', function(next){
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
+    this.invalidate('password');
+  }
+  next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = mongoose.model('User', userSchema);
